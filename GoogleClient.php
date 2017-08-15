329598bd4b6f7ea4c2ab975f13a8e30a055c83fe
Dharmin K Shah <6830654+dharmin@users.noreply.github.com>
2017-08-15 20:23:58 +0530

    refresh token added

--- GoogleClient.php
+++ GoogleClient.php
@@ -1,8 +1,10 @@
 <?php
-require_once 'vendor/autoload.php';
 
-class GoogleClient{
-    protected $client_id = '<CLIENT_ID>';
+require 'vendor/autoload.php';
+
+class GoogleClient
+{
+ protected $client_id = '<CLIENT_ID>';
     protected $client_secret = '<CLIENT_SECRET>';
     protected $redirect_uri = '<REDIRECTED_URI>';
     
@@ -12,34 +14,57 @@ class GoogleClient{
     protected $service;
 
     /**
-     *  Construct an easy to use API client.
+     *  Construct an easy to use Google API client.
      */
-    public function __construct() {
-        $this->client = new Google_Client();  
+    public function __construct()
+    {
+        $this->client = new \Google_Client();
         $this->client->setClientId($this->client_id);
         $this->client->setClientSecret($this->client_secret);
         $this->client->setRedirectUri($this->redirect_uri);
         $this->client->setAccessType('offline');
         $this->client->setScopes($this->scopes);
-        if(isset($_SESSION['GOOGLE_ACCESS_TOKEN'])){
+        if (isset($_SESSION['GOOGLE_ACCESS_TOKEN'])) {
             $this->client->setAccessToken($_SESSION['GOOGLE_ACCESS_TOKEN']);
+            //  Checking current access token is expired
+            if($this->client->isAccessTokenExpired()){
+                // Refresh access token and add it into session
+                $client->refreshToken($_SESSION['GOOGLE_REFRESH_TOKEN']);
+                $access_token = $this->client->getAccessToken();
+                $_SESSION['GOOGLE_ACCESS_TOKEN'] = $access_token;
+            }
         }
     }
 
     /**
-     *  Authenticate the client and set access token after login
+     *   Check if the user is logged in or not
+     */
+    public function isLoggedIn()
+    {
+        if (isset($_SESSION['GOOGLE_ACCESS_TOKEN'])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     *  Authenticate the client and set access token and refresh after login
      *  @param string $code redirected code
      */
-    public function authenticate($code) {
-        $this->client->authenticate($_GET['code']);
+    public function authenticate($code)
+    {
+        $this->client->authenticate($code);
         $_SESSION['GOOGLE_ACCESS_TOKEN'] = $this->client->getAccessToken();
+        $_SESSION['GOOGLE_REFRESH_TOKEN'] =  $this->client->getRefreshToken();
     }
 
     /**
      *  To set access token explicitely
      *  @param string $accessToken access token
      */
-    public function setAccessToken($accessToken){
+    public function setAccessToken($accessToken)
+    {
         $this->client->setAccessToken($accessToken);
     }
 
@@ -47,7 +72,8 @@ class GoogleClient{
      *  To get authentication URL if not in session
      *  @return string
      */
-    public function getAuthURL(){
+    public function getAuthUrl()
+    {
         return $this->client->createAuthUrl();
     }
 
@@ -55,15 +81,17 @@ class GoogleClient{
      *  Returns the google client object
      *  @return Google_Client
      */
-    public function getClient(){
+    public function getClient()
+    {
         return $this->client;
     }
 
     /**
      *  Initilize drive services object
      */
-    public function initDriveService(){
-        $this->service = new Google_Service_Drive($this->client);
+    public function initDriveService()
+    {
+        $this->service = new \Google_Service_Drive($this->client);
     }
 
     /**
@@ -72,11 +100,15 @@ class GoogleClient{
      *  @param string $folderName folder name to create
      *  @return string id of created folder
      */
-    public function createFolder($parentId,$folderName){
-        $fileMetadata = new Google_Service_Drive_DriveFile(array(
+    public function createFolder($parentId, $folderName)
+    {
+        // Setting File Matadata
+        $fileMetadata = new \Google_Service_Drive_DriveFile(array(
             'name' => $folderName,
             'parents' => array($parentId),
             'mimeType' => 'application/vnd.google-apps.folder'));
+        
+        // Creating Folder with given Matadata and asking for ID field as result
         $file = $this->service->files->create($fileMetadata, array('fields' => 'id'));
         return $file->id;
     }
@@ -84,39 +116,57 @@ class GoogleClient{
     /**
      *  Get the list of files or folders or both from given folder or root
      *  @param string $search complete or partial name of file or folder to search
-     *  @param string $parentId parent folder id or root from which the list of files or folders or both will be generated
+     *  @param string $parentId parent folder id or root from which the list of
+                                files or folders or both will be generated
      *  @param string $type='all' file or folder
      *  @return array list of files or folders or both from given parent directory
      */
-    public function listFilesFolders($search,$parentId,$type='all'){
+    public function listFilesFolders($search, $parentId, $type = 'all')
+    {
         $query = '';
+        // Checking if search is empty the use 'contains' condition if search is empty (to get all files or folders).
+        // Otherwise use '='  condition
         $condition = $search!=''?'=':'contains';
+        
+        // Search all files and folders otherwise search in root or  any folder
         $query .= $parentId!='all'?"'".$parentId."' in parents":"";
         
-        switch($type){
-          case "files":   $query .= $query!=''?' and ':'';
-          $query .= "mimeType != 'application/vnd.google-apps.folder' and name ".$condition." '".$search."'";
-          break;
-          case "folders": $query .= $query!=''?' and ':'';
-          $query .= "mimeType = 'application/vnd.google-apps.folder' and name contains '".$search."'";
-          break;
-          default:        $query .= "";
-          break;
-      }
-      $query .= $query!=''?' and trashed = false':'trashed = false';
-      $optParams = array('q' => $query,'pageSize' => 1000);
+        // Check if want to search files or folders or both
+        switch ($type) {
+            case "files":
+                $query .= $query!=''?' and ':'';
+                $query .= "mimeType != 'application/vnd.google-apps.folder' 
+                            and name ".$condition." '".$search."'";
+                break;
 
-      $results = $this->service->files->listFiles($optParams);
+            case "folders":
+                $query .= $query!=''?' and ':'';
+                $query .= "mimeType = 'application/vnd.google-apps.folder' and name contains '".$search."'";
+                break;
+            default:
+                $query .= "";
+                break;
+        }
 
-      if (count($results->getFiles()) == 0) {
-          return false;
-      }
-      $result = array();
-      foreach ($results->getFiles() as $file) {
-          $result[$file->getId()] = $file->getName();
-      }
-      return $result;
-  }
+        // Make sure that not list trashed files
+        $query .= $query!=''?' and trashed = false':'trashed = false';
+        $optParams = array('q' => $query,'pageSize' => 1000);
+
+        // Returns the list of files and folders as object
+        $results = $this->service->files->listFiles($optParams);
+
+        // Return false if nothing is found
+        if (count($results->getFiles()) == 0) {
+            return array();
+        }
+
+        // Converting array to object
+        $result = array();
+        foreach ($results->getFiles() as $file) {
+            $result[$file->getId()] = $file->getName();
+        }
+        return $result;
+    }
 
 
 
@@ -127,19 +177,30 @@ class GoogleClient{
      *  @param string $fileName file name of the uploaded copy at google drive
      *  @return string id of uploaded file
      */
-    public function uploadFile($parentId,$filePath,$fileName="none"){
-        if($fileName=="none") $fileName = end(explode('/', $filePath));
-            $fileMetadata = new Google_Service_Drive_DriveFile(array(
-                'name' => $fileName,
-                'parents' => array($parentId)
-            ));
-            $content = file_get_contents($filePath);
-            $file = $this->service->files->create($fileMetadata, array(
-                'data' => $content,
-                'mimeType' => 'image/jpeg',
-                'uploadType' => 'multipart',
-                'fields' => 'id'));
-            return $file->id;
+    public function uploadFile($parentId, $filePath, $fileName = "none")
+    {
+        // If file name is 'none' then give the orignal file name
+        if ($fileName=="none") {
+            $fileName = end(explode('/', $filePath));
         }
+
+        // Creating file matadata
+        $fileMetadata = new \Google_Service_Drive_DriveFile(array(
+            'name' => $fileName,
+            'parents' => array($parentId)
+        ));
+
+        // Getting file into variable
+        $content = file_get_contents($filePath);
+
+        // Uploading file and getting uploaded file ID as result
+        $file = $this->service->files->create($fileMetadata, array(
+            'data' => $content,
+            'mimeType' => 'image/jpeg',
+            'uploadType' => 'multipart',
+            'fields' => 'id'));
+        
+        // Returning file id of newly uploaded file
+        return $file->id;
     }
 }
